03915926726723ad1c231727904566dee2867a2c
mira <mkyshchak@gmail.com>
2025-04-07 22:25:45 +0300

    az16

--- task.ps1
+++ task.ps1
@@ -1,30 +1,101 @@
 $location = "uksouth"
-$resourceGroupName = "mate-azure-task-16"
-
+$resourceGroupName = "mate-azure-task-15"
 $virtualNetworkName = "todoapp"
-$vnetAddressPrefix = "10.20.30.0/24"
+
 $webSubnetName = "webservers"
-$webSubnetIpRange = "10.20.30.0/26"
 $dbSubnetName = "database"
-$dbSubnetIpRange = "10.20.30.64/26"
 $mngSubnetName = "management"
-$mngSubnetIpRange = "10.20.30.128/26"
 
+# Отримуємо існуючу віртуальну мережу та підмережі
+Write-Host "Getting existing virtual network and subnets..."
+$vnet = Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName
+$webSubnet = Get-AzVirtualNetworkSubnetConfig -Name $webSubnetName -VirtualNetwork $vnet
+$dbSubnet = Get-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -VirtualNetwork $vnet
+$mngSubnet = Get-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -VirtualNetwork $vnet
 
-Write-Host "Creating a resource group $resourceGroupName ..."
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+# Створюємо NSG-и
+Write-Host "Creating NSGs..."
+$webNsg = New-AzNetworkSecurityGroup -Name $webSubnetName -ResourceGroupName $resourceGroupName -Location $location
+$dbNsg = New-AzNetworkSecurityGroup -Name $dbSubnetName -ResourceGroupName $resourceGroupName -Location $location
+$mngNsg = New-AzNetworkSecurityGroup -Name $mngSubnetName -ResourceGroupName $resourceGroupName -Location $location
 
-Write-Host "Creating web network security group..."
-# Write your code for creation of Web NSG here -> 
+# Загальне правило для всіх NSG (дозволити трафік всередині віртуальної мережі)
+$commonRule = @{
+    Name = "Allow-VNet-Internal"
+    Protocol = "*"
+    Direction = "Inbound"
+    Priority = 100
+    SourceAddressPrefix = "VirtualNetwork"
+    SourcePortRange = "*"
+    DestinationAddressPrefix = "VirtualNetwork"
+    DestinationPortRange = "*"
+    Access = "Allow"
+}
 
-Write-Host "Creating mngSubnet network security group..."
-# Write your code for creation of management NSG here -> 
+# Правила для web NSG
+$webHttpRule = @{
+    Name = "Allow-HTTP"
+    Protocol = "Tcp"
+    Direction = "Inbound"
+    Priority = 200
+    SourceAddressPrefix = "Internet"
+    SourcePortRange = "*"
+    DestinationAddressPrefix = "*"
+    DestinationPortRange = "80"
+    Access = "Allow"
+}
+$webHttpsRule = @{
+    Name = "Allow-HTTPS"
+    Protocol = "Tcp"
+    Direction = "Inbound"
+    Priority = 201
+    SourceAddressPrefix = "Internet"
+    SourcePortRange = "*"
+    DestinationAddressPrefix = "*"
+    DestinationPortRange = "443"
+    Access = "Allow"
+}
 
-Write-Host "Creating dbSubnet network security group..."
-# Write your code for creation of management NSG here -> 
+# Правила для management NSG
+$mngSshRule = @{
+    Name = "Allow-SSH"
+    Protocol = "Tcp"
+    Direction = "Inbound"
+    Priority = 200
+    SourceAddressPrefix = "Internet"
+    SourcePortRange = "*"
+    DestinationAddressPrefix = "*"
+    DestinationPortRange = "22"
+    Access = "Allow"
+}
 
-Write-Host "Creating a virtual network ..."
-$webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange
-$dbSubnet = New-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -AddressPrefix $dbSubnetIpRange
-$mngSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange
-New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $webSubnet,$dbSubnet,$mngSubnet
+# Додаємо правила
+Add-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $webNsg @commonRule
+Add-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $webNsg @webHttpRule
+Add-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $webNsg @webHttpsRule
+
+Add-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $dbNsg @commonRule
+
+Add-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $mngNsg @commonRule
+Add-AzNetworkSecurityRuleConfig -NetworkSecurityGroup $mngNsg @mngSshRule
+
+# Оновлюємо NSG-и
+$webNsg | Set-AzNetworkSecurityGroup
+$dbNsg | Set-AzNetworkSecurityGroup
+$mngNsg | Set-AzNetworkSecurityGroup
+
+# Прив’язуємо NSG-и до підмереж
+Write-Host "Associating NSGs with subnets..."
+$webSubnet.NetworkSecurityGroup = $webNsg
+$dbSubnet.NetworkSecurityGroup = $dbNsg
+$mngSubnet.NetworkSecurityGroup = $mngNsg
+
+# Оновлюємо підмережі у віртуальній мережі
+Set-AzVirtualNetwork -VirtualNetwork $vnet
+
+# Запуск скриптів
+Write-Host "Running artifacts generation script..."
+.\scripts\generate-artifacts.ps1
+
+Write-Host "Running validation script..."
+.\scripts\validate-artifacts.ps1
